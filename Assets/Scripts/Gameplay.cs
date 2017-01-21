e2acc4158f843192f24b66cfcd000c157f565759
Felipe Rocha Wagner <feliperw@msn.com>
2017-01-21 01:11:17 -0200

    Controle do jogador(Player e Gameplay), Prefab da Interface, Cena base.

---
+++ Assets/Scripts/Gameplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour
+{
+    public Player player;
+    public void PlayPunk()
+    {
+        if (!player.usingKeyboard)
+            player.PlayPunk();
+    }
+    public void PlayClassic()
+    {
+        if (!player.usingKeyboard)
+            player.PlayClassic();
+    }
+    public void PlayReggae()
+    {
+        if (!player.usingKeyboard)
+            player.PlayReggae();
+    }
+    public void PlayEletronic()
+    {
+        if (!player.usingKeyboard)
+            player.PlayEletronic();
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //[System.NonSerialized]
+    //public Animator animator;
+    public bool usingKeyboard = false;
+    public float life = 5f;
+    void Start()
+    {
+        //Animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            usingKeyboard = true;
+            PlayPunk(); //Tocando Punk
+        }
+        else if (Input.GetKey(KeyCode.DownArrow)) 
+        {
+            usingKeyboard = true;
+            PlayClassic(); //Tocando Clássico
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)) 
+        {
+            usingKeyboard = true;
+            PlayReggae(); //Tocando Reggae
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow)) 
+        {
+            usingKeyboard = true;
+            PlayEletronic(); //Tocando Eletrônica
+        }
+        else //Idle
+        {
+            usingKeyboard = false;
+        }
+    }
+    public void PlayPunk()
+    {
+    }
+    public void PlayClassic()
+    {
+    }
+    public void PlayReggae()
+    {
+    }
+    public void PlayEletronic()
+    {
+    }
+}
