76a9f197fc186c55cec6e530ba3f342ba1c6f96f
gabriellanzer <gabriellanzerlive@hotmail.com>
2017-01-21 18:26:14 -0200

    Wave scale fixed

--- Assets/WaveGenerator/Wave.cs
+++ Assets/WaveGenerator/Wave.cs
@@ -8,21 +8,22 @@ public class Wave : MonoBehaviour {
 	public float lifeSpan = 1f;
 	public float targetSize = 1f;
 	public float life = 0f;
+    public Vector3 baseScale;
 
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () 
+	void OnStart() {
+        baseScale = new Vector3(0.5f,0.15f,0.5f);
+    }
+
+    // Update is called once per frame
+    void Update () 
 	{
 		life += Time.deltaTime;
 		if (life >= lifeSpan) {
 			myPool.addToPool (this.gameObject);
 		}
 		Vector3 sca = this.transform.localScale;
-		sca += new Vector3 (Time.deltaTime * targetSize/lifeSpan, Time.deltaTime * targetSize/lifeSpan, Time.deltaTime * targetSize/lifeSpan);
+		sca += new Vector3 (Time.deltaTime * targetSize/lifeSpan, Time.deltaTime * targetSize/lifeSpan*0.15f, Time.deltaTime * targetSize/lifeSpan);
 		this.transform.localScale = sca;
 		Color col = this.GetComponent<SpriteRenderer> ().color;
 		col.a -= Time.deltaTime / lifeSpan;
