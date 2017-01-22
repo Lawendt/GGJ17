d57446e2df64da2691f8df58d38359bfce43756b
Leonardo Santana <leonardogsantana@live.com>
2017-01-22 01:04:22 -0200

    Add EndGame

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void LoadingScene(string Name)
+    {
+        SceneManager.LoadSceneAsync(Name);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
