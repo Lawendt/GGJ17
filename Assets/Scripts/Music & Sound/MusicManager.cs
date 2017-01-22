71cab0538281be34d6eed4694eabdc47bda627bd
gabriellanzer <gabriellanzerlive@hotmail.com>
2017-01-21 22:55:53 -0200

    Music and Sound Manager #1, new Palco.jpg

---
+++ Assets/Scripts/Music & Sound/MusicManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+using System;
+
+public class MusicManager : Singleton<MusicManager>
+{
+    public AudioSource[] sources;
+    public AudioClip[] tracks;
+    int lastTrack;
+
+    void fadeOut(AudioSource source, Action action = null)
+    {
+        StartCoroutine(_fadeOut(source, action));
+    }
+
+    void fadeIn(AudioSource source, Action action = null)
+    {
+        StartCoroutine(_fadeIn(source, action));
+    }
+
+    IEnumerator _fadeOut(AudioSource source, Action action = null)
+    {
+        while (source.volume > 0.01)
+        {
+            source.volume = Mathf.Lerp(source.volume, 0, 0.01f);
+            yield return new WaitForEndOfFrame();
+        }
+
+        source.Stop();
+        if (action != null)
+            action();
+
+    }
+
+    IEnumerator _fadeIn(AudioSource source, Action action = null)
+    {
+        while (source.volume < 0.91)
+        {
+            source.volume = Mathf.Lerp(source.volume, 1, 0.01f);
+            yield return new WaitForEndOfFrame();
+        }
+
+        source.volume = 1f;
+        if (action != null)
+            action();
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        _dontDestroyOnLoad = true;
+        _destroyExistentObject = true;
+    }
+
+
+    void ChangeTrackTo(AudioClip clip)
+    {
+        StopAllCoroutines();
+        
+    }
+
+}
---
+++ Assets/Scripts/Music & Sound/SoundManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : Singleton<SoundManager> {
+
+    public AudioSource[] sources;
+    public AudioClip[] effects;
+
+    void Start()
+    {
+        _destroyExistentObject = true;
+    }
+
+    void play(AudioClip clip)
+    {
+        if (sources == null)
+            return;
+        if (sources.Length == 0)
+            return;
+        if (clip == null)
+            return;
+
+        AudioSource source = null;
+        for (int i = 0; i < sources.Length; i++)
+        {
+            if (!sources[i].isPlaying)
+                source = sources[i];
+        }
+
+        if (source == null)
+            source = sources[0];
+
+        source.pitch = Random.Range(0.8f, 1.2f);
+        source.clip = clip;
+        source.PlayOneShot(clip);
+    }
+}
